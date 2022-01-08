e19eb7b48094a40fcde331a2657bf27a3f83bf44
Etienne TRAN <etiennetran@hotmail.fr>
2022-01-08 18:39:03 +0100

    Début projet

---
+++ src/components/ArticleTable.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Skeleton from '@mui/material/Skeleton';
+
+const data = [
+    {
+        src: 'https://i.ytimg.com/vi/pLqipJNItIo/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBkklsyaw9FxDmMKapyBYCn9tbPNQ',
+        title: 'Don Diablo @ Tomorrowland Main Stage 2019 | Official…',
+        channel: 'Don Diablo',
+        views: '396 k views',
+        createdAt: 'a week ago',
+    },
+    {
+        src: 'https://i.ytimg.com/vi/_Uu12zY01ts/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCpX6Jan2rxrCAZxJYDXppTP4MoQA',
+        title: 'Queen - Greatest Hits',
+        channel: 'Queen Official',
+        views: '40 M views',
+        createdAt: '3 years ago',
+    },
+    {
+        src: 'https://i.ytimg.com/vi/kkLk2XWMBf8/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLB4GZTFu1Ju2EPPPXnhMZtFVvYBaw',
+        title: 'Calvin Harris, Sam Smith - Promises (Official Video)',
+        channel: 'Calvin Harris',
+        views: '130 M views',
+        createdAt: '10 months ago',
+    },
+];
+
+export default class ArticleTable extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+    }
+
+    getData = () => {
+        const url = `${process.env.REACT_APP_API_HOST}/api/articles?api_token=n0ZxKC8alPqVw7paujQXai42QOUVY8widhdQW4DWPToBgMXbB73cvlKj6vIqsEwfz8HfqwA0JOnOW0dZ`;
+
+        fetch(url).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Request failed!');
+        }, networkError => {
+            console.log(networkError.message)
+        })
+    }
+
+    componentDidMount = () => {
+        this.setState({ data: this.getData() });
+    }
+
+    render() {
+        const { loading = false } = this.props;
+        return (
+            <Grid container wrap="nowrap">
+                {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
+                    <Box key={index} sx={{ width: 210, marginRight: 0.5, my: 5 }}>
+                        {item ? (
+                            <img
+                                style={{ width: 210, height: 118 }}
+                                alt={item.title}
+                                src={item.src}
+                            />
+                        ) : (
+                            <Skeleton variant="rectangular" width={210} height={118} />
+                        )}
+
+                        {item ? (
+                            <Box sx={{ pr: 2 }}>
+                                <Typography gutterBottom variant="body2">
+                                    {item.title}
+                                </Typography>
+                                <Typography display="block" variant="caption" color="text.secondary">
+                                    {item.channel}
+                                </Typography>
+                                <Typography variant="caption" color="text.secondary">
+                                    {`${item.views} • ${item.createdAt}`}
+                                </Typography>
+                            </Box>
+                        ) : (
+                            <Box sx={{ pt: 0.5 }}>
+                                <Skeleton />
+                                <Skeleton width="60%" />
+                            </Box>
+                        )}
+                    </Box>
+                ))}
+            </Grid>
+        )
+    }
+}
